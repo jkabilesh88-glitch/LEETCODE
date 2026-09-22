8a5734920bb156a765520d83c93c81d2cf864764
KABILESH JAYAKUMAR <jkabilesh88@gmail.com>
2026-09-22 21:48:02 +0530

    Time: 0 ms (100%), Space: 12.6 MB (26.47%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+       int max1=*max_element(candies.begin(),candies.end());
+       vector<bool>result(candies.size());
+       for(int i=0;i<candies.size();i++)
+       {
+        if(candies[i]+extraCandies>=max1)
+        {
+            result[i]=true;
+        }
+        else{
+            result[i]=false;
+        }
+       }
+return result;
+    }
+};
